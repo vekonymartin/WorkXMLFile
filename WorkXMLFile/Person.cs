0035ab048ea9b32dbaf1c0ad760e9d9aabac4937
vekonymartin <vekonymartin@stud.uni-obuda.hu>
2019-09-30 21:20:03 +0200

    Create file

---
+++ WorkXMLFile/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq; // it's need to work xml file
+
+namespace WorkXMLFile
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Dept { get; set; }
+        public string Rank { get; set; }
+        public string Phone { get; set; }
+        public string Room { get; set; }
+
+        public static List<Person> Load(string url)
+        {
+            XDocument xdoc = XDocument.Load(url);
+            return xdoc.Descendants("person").Select(
+                node => new Person()
+                {
+                    Name = node.Element("name")?.Value,
+                    Email = node.Element("email")?.Value,
+                    Dept = node.Element("dept")?.Value,
+                    Rank = node.Element("rank")?.Value,
+                    Phone = node.Element("phone")?.Value,
+                    Room = node.Element("room")?.Value
+                }).ToList();
+        }
+    }
+}
---
+++ WorkXMLFile/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkXMLFile
+{
+    class Program
+    {
+        static void ToConsole<T>(IEnumerable<T> input, string str)
+        {
+            Console.WriteLine("***" + str);
+
+            foreach (T item in input)
+                Console.WriteLine(item.ToString());
+
+            Console.WriteLine("***" + str + "end");
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            List<Person> people = Person.Load("http://users.nik.uni-obuda.hu/prog3/_data/people.xml");
+
+            var names = people.Where(p => p.Dept == "Biomatika Intézet").Select(p => new { p.Name, p.Rank });
+            ToConsole(names, "0. feladat");
+
+            // ======================================================================
+            // 1. Task
+            // Határozzuk meg az AII intézetben dolgozók darabszámát!
+
+            string dept = "Alkalmazott Informatikai Intézet";
+            var f1 = people.Where(p => p.Dept == dept).Count();
+
+            // ======================================================================
+            // 2. Task
+            // Jelenítsük meg azokat, akiknek a harmadik emeleten van irodája!
+
+
+
+
+            // ======================================================================
+            // 3. Task
+            // Kiknek van a leghosszabb vagy legrövidebb nevük?
+
+
+
+            // ======================================================================
+            // 4. Task
+            // Határozzuk meg intézetenként a dolgozók darabszámát!
+
+
+
+
+
+            // ======================================================================
+            // 5. Task
+            // Határozzuk meg a legnagyobb intézetet!
+
+
+
+
+            // ======================================================================
+            // 6. Task
+            // Listázzuk a legnagyobb intézet dolgozóit!
+
+
+
+
+            // ======================================================================
+            // 7. Task
+            // Listázzuk a harmadik legnagyobb intézet dolgozóit szobaszám szerint csökkenő sorrendben!
+
+        }
+    } 
+}
